d22da4b658f76c98348f05e7957fb03fd9dc1e19
Hugo Benjumea <hugo_andresb@hotmail.com>
2014-10-31 22:54:11 +0000

    Se ajustan vistas de member y work

--- app/views/members/index.blade.php
+++ app/views/members/index.blade.php
@@ -1,1 +1,27 @@
-/var/www/html/caanguila/teamwork/app/views/members/index.blade.php
+@extends('layouts.default')
+@section('content')
+  <h1>Recent members</h1>
+
+  {{ HTML::linkAction('members.create','New member',[],['class'=>'button']) }}
+
+  @foreach($members as $member)
+    <div class="row">
+      <div class="columns large-11">
+        {{ HTML::linkAction('members.show',$member->name . ' ' . $member->lastname,[$member->id],[]) }}<br>
+		
+        {{ $member->email }}
+      </div>
+      <div class="columns large-1">
+        {{ HTML::linkAction('members.edit','Edit',[$member->id],['class'=>'button tiny']) }}
+        {{ Form::model($member, ['route' => ['members.destroy', $member->id], 'method' => 'DELETE' ]) }}
+			{{ Form::button('Destroy', ['type' => 'submit', 'class' => 'tiny alert button'])}}
+			{{ $errors->first('members.show','<small class="error">:message</small>') }}
+		{{ Form::close() }}
+      </div>
+    </div>
+    <hr>
+  @endforeach
+   @if(Session::has('errorFk'))
+        <small class="error">{{ Session::get('errorFk') }}</small>
+    @endif
+@stop
